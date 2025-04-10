18211c3ef28e62c58af57bd09ec075ee40b070b4
Léo Peyronnet <peyronnet.leo@gmail.com>
2025-04-10 10:03:08 +0200

    feat: idle

--- js/hanoi-recursive.js
+++ js/hanoi-recursive.js
@@ -9,28 +9,62 @@ const colors = [
     "#ffd700", // or
     "#8a2be2", // bleu violet
     "#00ced1"  // turquoise moyen
-  ];
+];
 
+const tours = document.querySelectorAll(".tour");
+const nb_plates_input = document.querySelector("#nb-plates");
+const idle_speed_input = document.querySelector("#idle-speed");
+const launch_idle_button = document.querySelector("#launch-idle");
 
-function initPile(h) {
-    let pile = [];
-    for (let i = h ; i > 0 ; i--) {
-        pile.push(i);
+const stop_restart_idle_button = document.querySelector("#stop-restart-idle");
+const stop_icon = stop_restart_idle_button.querySelector("#stop-icon");
+const restart_icon = stop_restart_idle_button.querySelector("#restart-icon");
+
+let nb_plates;
+let is_running;
+
+function init() {
+    // Update variables
+    nb_plates = nb_plates_input.value;
+
+    // Update buttons
+    if (nb_plates != 0) {
+        idle_speed_input.disabled = false;
+        launch_idle_button.disabled = false;
+        stop_restart_idle_button.disabled = true;
+    } 
+    else {
+        idle_speed_input.disabled = true;
+        launch_idle_button.disabled = true;
+        stop_restart_idle_button.disabled = true;
     }
-    return pile;
+
+    // Update DOM
+    tours.forEach(tour => {
+        tour.querySelectorAll(".plate").forEach(plate => plate.remove());
+    });
+    for (let i = 1; i <= nb_plates; i++){
+        tours[0].appendChild(createPlateDOM(i));
+    }
 }
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-async function hanoi(n,from,to,pivot){
+async function hanoi(n, from, to, pivot) {
+    if (!is_running) return;
+    
     if (n == 1) {
-        await movePlate(from,to);
+        await movePlate(from, to);
     } else {
-        await hanoi(n-1,from,pivot,to);
-        await movePlate(from,to);
-        await hanoi(n-1,pivot,to,from);
+        await hanoi(n-1, from, pivot, to);
+        if (!is_running) return;
+        
+        await movePlate(from, to);
+        if (!is_running) return;
+        
+        await hanoi(n-1, pivot, to, from);
     }
 }
 
@@ -57,54 +91,35 @@ async function movePlate(from, to){
     await sleep(500 / idle_speed_input.value);
 }
 
-const tours = document.querySelectorAll(".tour");
-const nb_plates_input = document.querySelector("#nb-plates");
-const idle_speed_input = document.querySelector("#idle-speed");
-const launch_idle_button = document.querySelector("#launch-idle");
-const pause_idle_button = document.querySelector("#pause-idle");
-const step_back_button = document.querySelector("#step-back");
-const step_forward_button = document.querySelector("#step-forward");
 
-let tours_listes;
-let nb_plates;
 
-nb_plates_input.addEventListener("change", () => {
-    // Update variables
-    nb_plates = nb_plates_input.value;
-    tours_listes = [initPile(nb_plates),[],[]];
+nb_plates_input.addEventListener("change", init);
 
-    // Update buttons
-    if (nb_plates_input.value != 0) {
-        idle_speed_input.disabled = false;
-        launch_idle_button.disabled = false;
-        pause_idle_button.disabled = false;
-        step_back_button.disabled = false;
-        step_forward_button.disabled = false;
-    } 
-    else {
-        idle_speed_input.disabled = true;
-        launch_idle_button.disabled = true;
-        pause_idle_button.disabled = true;
-        step_back_button.disabled = true;
-        step_forward_button.disabled = true;
+
+launch_idle_button.addEventListener("click", async function(){
+    launch_idle_button.disabled = true;
+    stop_restart_idle_button.disabled = false;
+
+    is_running = true;
+    await hanoi(nb_plates,0,2,1);
+
+    if (is_running){ // If normal run , not stoped by user input
+        stop_icon.classList.add("hidden"); // hide stop icon
+        restart_icon.classList.remove("hidden"); // show restart icon
     }
 
-    // Update DOM
-    tours.forEach(tour => {
-        tour.querySelectorAll(".plate").forEach(plate => plate.remove());
-    });
-    for (let i = 1; i <= nb_plates; i++){
-        tours[0].appendChild(createPlateDOM(i));
+    is_running = false;
+
+});
+
+stop_restart_idle_button.addEventListener("click", () => {
+    stop_restart_idle_button.disabled = true;
+
+    if (!is_running){ // If "restart button"
+        restart_icon.classList.add("hidden"); // hide restart icon
+        stop_icon.classList.remove("hidden"); // show stop icon
     }
-});
 
-launch_idle_button.addEventListener("click", () => {
-    step_back_button.disabled = true;
-    step_forward_button.disabled = true;
-    hanoi(nb_plates,0,2,1);
-});
-
-pause_idle_button.addEventListener("click", () => {
-    step_back_button.disabled = false;
-    step_forward_button.disabled = false;
-});
+    is_running = false;
+    init();
+}); 
